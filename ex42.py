dd31839ce38b62e4b28d90a8b18898785d63e912
ozlop <oswaldo.t.lopez@gmail.com>
2017-11-05 15:15:13 -0800

    Exercise 42. Is-A, Has-A, Objects, and Classes

---
+++ ex42.py
@@ -0,0 +1,76 @@
+## Animal is-a object
+class Animal(object)
+    pass
+
+## Dog is-a Animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## from self, get name attribute and set it to self
+        self.name = name
+
+## Cat is-a animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ##
+        self.name = name
+
+## Person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        ## Person has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ##
+        super(Employee, self).__init__(name)
+        ## from self, get salary and set it to salary
+        self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+    pass
+
+## Salmon is-a Fish
+class Salmon(Fish):
+    pass
+
+## Halibut is-a Fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## satan is-a Cat
+satan = Cat("Satan")
+
+## mary is-a Person
+mary = Person("Mary")
+
+## mary has-a pet, satan
+mary.pet = satan
+
+## frank is-a Employee
+frank = Employee("Frank", 120000)
+
+## frank has-a pet
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish()
+
+## crouse is-a Salmon
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut()
